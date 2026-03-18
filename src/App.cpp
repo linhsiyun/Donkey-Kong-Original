ac0a4d6b2b992d32da9e6db121c5fcc7e6aa7378
112590452 <t112590452@ntut.org.tw>
2026-03-19 03:51:29 +0800

    feat: implement map loading and tile rendering

--- src/App.cpp
+++ src/App.cpp
@@ -7,13 +7,22 @@
 
 void App::Start() {
     LOG_TRACE("Start");
+
+    // 1. 載入地圖並取得物件
+    auto mapTiles = m_Map.LoadMap("../Resources/Maps/Map1.txt"); // 請換成你的 txt 路徑
+
+    // 2. 把地圖物件交給 Renderer 管理
+    m_Renderer.AddChildren(mapTiles);
+
     m_CurrentState = State::UPDATE;
 }
 
 void App::Update() {
     
     //TODO: do your things here and delete this line <3
-    
+
+    // 讓 Renderer 每一幀自動畫出所有東西
+    m_Renderer.Update();
     /*
      * Do not touch the code below as they serve the purpose for
      * closing the window.
