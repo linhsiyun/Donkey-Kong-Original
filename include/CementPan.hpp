518c3a586f17cb89e5eca94a65409958f973d6b7
邱郁晴 <t113ab0018@ntut.org.tw>
2026-06-19 23:47:24 +0800

    Fix bug: Fireball Not Moving + Wait for Victory

--- include/CementPan.hpp
+++ include/CementPan.hpp
@@ -33,8 +33,7 @@ public:
 
         // 1. 偵測腳下的 Tile
         TileType currentTile = map->GetTileAtPosition(pos.x, footY - 5.0f);
-        TileType deeperTile  = map->GetTileAtPosition(pos.x, footY - 20.0f);
-        if (currentTile == TileType::EMPTY && deeperTile == TileType::EMPTY) {
+        if (currentTile == TileType::EMPTY) {
             // 2. 只有在地圖內偵測到「空位」(EMPTY) 時才移除，代表掉進洞裡
             m_ShouldRemove = true;
             return;
